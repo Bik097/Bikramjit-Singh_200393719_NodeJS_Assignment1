570eee4f1da357dc52ec27aae2f3538573e83b3b
Bik097 <bjs241997@gmail.com>
2019-10-29 14:34:55 -0400

    first commit

---
+++ routes/services.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('services', { title: 'Services' , portfolio: "Bik's profile"});
+});
+
+module.exports = router;
